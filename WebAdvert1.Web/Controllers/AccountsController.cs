e7456a7ea0d316efb01048bd217ce77031803b85
tonbaharry <tubotonba.harry@aol.com>
2022-06-24 13:15:16 -0400

    first commit

---
+++ WebAdvert1.Web/Controllers/AccountsController.cs
@@ -0,0 +1,115 @@
+﻿using Amazon.Extensions.CognitoAuthentication;
+using Amazon.AspNetCore.Identity.Cognito;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAdvert1.Web.Models.Accounts;
+using Amazon.Runtime.Internal.Transform;
+
+namespace WebAdvert1.Web.Controllers
+{
+    
+    public class AccountsController : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+        public AccountsController(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+        public IActionResult SignUp()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if(user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+                user.Attributes.Add(CognitoAttribute.Name.ToString(), model.Email);  //lecture 11 28.13
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+                if(createdUser.Succeeded)
+                {
+                    return RedirectToAction("Confirm");
+                }
+            }
+            return View();
+        }
+
+        
+        public async Task<IActionResult> Confirm()
+        {
+            var model = new ConfirmModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if(user == null)
+                {
+                    ModelState.AddModelError("NotFound", "A user with the given email address was not found");
+                    return View(model);
+                }
+                var result = await (_userManager as CognitoUserManager<CognitoUser>).ConfirmSignUpAsync(user, model.Code, true).ConfigureAwait(false);
+                    //await _userManager.ConfirmEmailAsync(user, model.Code);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError(item.Code, item.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            var model = new LoginModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public async Task<IActionResult> Login_Post(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("LoginError", "Email and password do not match");
+                }
+            }
+            return View("Login", model);
+        }
+    }
+}
---
+++ WebAdvert1.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert1.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be at least six characters long!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and its confirmation not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
